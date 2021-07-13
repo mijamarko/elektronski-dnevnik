50a69fe195340e81593193b8d59e254a62421ea6
Marko Mijajlović <marko.b.mijajlovic@gmail.com>
2021-07-13 02:07:04 +0200

    removed izvestaj entity since it should be a dto and needs rethinking

--- src/main/java/com/iktpreobuka/elektronski_dnevnik/entities/reports/IzvestajEntity.java
+++
@@ -1,8 +0,0 @@
-package com.iktpreobuka.elektronski_dnevnik.entities.reports;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties({ "handler", "hibernateLazyInitializer" })
-public class IzvestajEntity {
-
-}
